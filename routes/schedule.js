008288e10e2219de1ee5dce662dd41d7e6c78c29
Conrelia Harder <cornelialisaharder@gmail.com>
2023-01-10 20:05:00 +0100

    updated schedule

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,89 +1,99 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require("mongodb").ObjectID;
 
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  req.app.locals.db
+    .collection("schedule")
+    .find()
+    .toArray()
+    .then((results) => {
+      let today = new Date();
+      let futureArray = [];
 
-    req.app.locals.db.collection("schedule").find().toArray().then(
-      results => {
-        console.log(results)
-    res.send(results);
-
-      }
-    )
-  
-  
-  });
-
-  //Create new schedule post
-  router.post("/add", function(req,res) {
-  
-    req.app.locals.db.collection("schedule").insertOne(
-{   
-    title:req.body.title,
-    when:req.body.when,
-    where:req.body.where,
-    conductor:req.body.conductor,
-    image_url:req.body.image_url,
-    read_more:req.body.read_more,
-    date_remove:req.body.date_remove
-
-}
-
-    ).then(result=>{
-
-   //If scheduled post is an Opera, an opera-repetoire post is created     
-        if(req.body.repetoire === "Opera") {
-            req.app.locals.db.collection("repetoire_opera").insertOne(
-                {   
-                    composer:req.body.composer,
-                    opera:req.body.opera,
-                    role:req.body.role,
-                    date_publish:req.body.date_remove,
-                    schedule_id:result.insertedId,
-                    display_repetoire:req.body.display_repetoire
-                }
-                
-                    ).then(result=>{
-                      console.log(result)
-                      res.send(result)
-                    })
-
-    ////If scheduled post is a Concert, an opera-repetoire post is created     
-        } else if (req.body.repetoire === "Concert") {
-            req.app.locals.db.collection("repetoire_concert").insertOne(
-                {   
-                    composer:req.body.composer,
-                    work:req.body.work,
-                    date_publish:req.body.date_remove,
-                    schedule_id:result.insertedId,
-                    display_repetoire:req.body.display_repetoire
-                }
-                
-                    ).then(result=>{
-                      res.send(result)
-                    })
+      for (let i = 0; i < results.length; i++) {
+        let dateFromDB = new Date(results[i].date_remove);
+        if (dateFromDB >= today) {
+          console.log(dateFromDB);
+          futureArray.push(results[i]);
         }
-      console.log(result)
+      }
+
+      res.send(futureArray);
+    });
+});
+
+//Create new schedule post
+router.post("/add", function (req, res) {
+  req.app.locals.db
+    .collection("schedule")
+    .insertOne({
+      title: req.body.title,
+      when: req.body.when,
+      where: req.body.where,
+      conductor: req.body.conductor,
+      image_url: req.body.image_url,
+      read_more: req.body.read_more,
+      date_remove: req.body.date_remove,
     })
-  
-  })
+    .then((result) => {
+      //If scheduled post is an Opera, an opera-repetoire post is created
+      if (req.body.repetoire === "Opera") {
+        req.app.locals.db
+          .collection("repetoire_opera")
+          .insertOne({
+            composer: req.body.composer,
+            opera: req.body.opera,
+            role: req.body.role,
+            date_publish: req.body.date_remove,
+            schedule_id: result.insertedId,
+            display_repetoire: req.body.display_repetoire,
+          })
+          .then((result) => {
+            console.log(result);
+            res.send(result);
+          });
 
-  router.post("/delete", function(req, res){
-    console.log(req.body)
-    req.app.locals.db.collection("schedule").deleteOne({"_id":ObjectId(req.body._id)}).then(result=>{
-        console.log(result)
-    })
+        ////If scheduled post is a Concert, an opera-repetoire post is created
+      } else if (req.body.repetoire === "Concert") {
+        req.app.locals.db
+          .collection("repetoire_concert")
+          .insertOne({
+            composer: req.body.composer,
+            work: req.body.work,
+            date_publish: req.body.date_remove,
+            schedule_id: result.insertedId,
+            display_repetoire: req.body.display_repetoire,
+          })
+          .then((result) => {
+            res.send(result);
+          });
+      }
+      console.log(result);
+    });
+});
 
-    req.app.locals.db.collection("repetoire_opera").deleteOne({"schedule_id":ObjectId(req.body._id)}).then(result=>{
-      console.log(result)
-  })
-  req.app.locals.db.collection("repetoire_concert").deleteOne({"schedule_id":ObjectId(req.body._id)}).then(result=>{
-    console.log(result)
-})
+router.post("/delete", function (req, res) {
+  console.log(req.body);
+  req.app.locals.db
+    .collection("schedule")
+    .deleteOne({ _id: ObjectId(req.body._id) })
+    .then((result) => {
+      console.log(result);
+    });
 
+  req.app.locals.db
+    .collection("repetoire_opera")
+    .deleteOne({ schedule_id: ObjectId(req.body._id) })
+    .then((result) => {
+      console.log(result);
+    });
+  req.app.locals.db
+    .collection("repetoire_concert")
+    .deleteOne({ schedule_id: ObjectId(req.body._id) })
+    .then((result) => {
+      console.log(result);
+    });
+});
 
-  })
-
-
-  module.exports = router;
+module.exports = router;
