3420bab369d439b90f7c19d425f59d4b7fa10160
Conrelia Harder <cornelialisaharder@gmail.com>
2023-02-08 14:32:32 +0100

    uppdating error response routes

--- routes/media.js
+++ routes/media.js
@@ -23,6 +23,55 @@ router.get("/", async function (req, res, next) {
           }
         }
         console.log(results);
+        // throw new Error("Hello Error!");
+        res.send(showArray);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
+});
+
+router.get("/all", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find()
+      .toArray()
+      .then((results) => {
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
+
+        res.send(sortedArray);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
+});
+
+router.get("/video", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "Video" })
+      .toArray()
+      .then((results) => {
+        let showArray = [];
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
+
+        for (let i = 0; i < sortedArray.length; i++) {
+          if (showArray.length < 4) {
+            showArray.push(sortedArray[i]);
+          }
+        }
 
         res.send(showArray);
       });
@@ -32,184 +81,185 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-router.get("/all", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find()
-    .toArray()
-    .then((results) => {
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
-      });
+router.get("/audio", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "Audio" })
+      .toArray()
+      .then((results) => {
+        let showArray = [];
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
 
-      res.send(sortedArray);
-    });
-});
-
-router.get("/video", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "Video" })
-    .toArray()
-    .then((results) => {
-      let showArray = [];
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
-      });
-
-      for (let i = 0; i < sortedArray.length; i++) {
-        if (showArray.length < 4) {
-          showArray.push(sortedArray[i]);
+        for (let i = 0; i < sortedArray.length; i++) {
+          if (showArray.length < 4) {
+            showArray.push(sortedArray[i]);
+          }
         }
-      }
 
-      res.send(showArray);
-    });
+        res.send(showArray);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
-router.get("/audio", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "Audio" })
-    .toArray()
-    .then((results) => {
-      let showArray = [];
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
-      });
+router.get("/news", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "News" })
+      .toArray()
+      .then((results) => {
+        let showArray = [];
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
 
-      for (let i = 0; i < sortedArray.length; i++) {
-        if (showArray.length < 4) {
-          showArray.push(sortedArray[i]);
+        for (let i = 0; i < sortedArray.length; i++) {
+          if (showArray.length < 4) {
+            showArray.push(sortedArray[i]);
+          }
         }
-      }
 
-      res.send(showArray);
-    });
+        res.send(showArray);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
-router.get("/news", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "News" })
-    .toArray()
-    .then((results) => {
-      let showArray = [];
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
+router.get("/video/all", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "Video" })
+      .toArray()
+      .then((results) => {
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
+
+        res.send(sortedArray);
       });
-
-      for (let i = 0; i < sortedArray.length; i++) {
-        if (showArray.length < 4) {
-          showArray.push(sortedArray[i]);
-        }
-      }
-
-      res.send(showArray);
-    });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
-router.get("/video/all", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "Video" })
-    .toArray()
-    .then((results) => {
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
-      });
+router.get("/audio/all", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "Audio" })
+      .toArray()
+      .then((results) => {
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
 
-      res.send(sortedArray);
-    });
+        res.send(sortedArray);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
-router.get("/audio/all", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "Audio" })
-    .toArray()
-    .then((results) => {
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
+router.get("/news/all", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .find({ type: "News" })
+      .toArray()
+      .then((results) => {
+        let sortedArray = results.sort(function compare(a, b) {
+          var dateA = new Date(a.date_pub);
+          var dateB = new Date(b.date_pub);
+          return dateB - dateA;
+        });
+
+        res.send(sortedArray);
       });
-
-      res.send(sortedArray);
-    });
-});
-
-router.get("/news/all", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .find({ type: "News" })
-    .toArray()
-    .then((results) => {
-      let sortedArray = results.sort(function compare(a, b) {
-        var dateA = new Date(a.date_pub);
-        var dateB = new Date(b.date_pub);
-        return dateB - dateA;
-      });
-
-      res.send(sortedArray);
-    });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
 //Add new media
-router.post("/add", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .insertOne(req.body)
-    .then((result) => {
-      console.log(result);
-      res.send(result);
-    });
+router.post("/add", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .insertOne(req.body)
+      .then((result) => {
+        console.log(result);
+
+        res.send(result);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
 //Delete post
-router.post("/delete", function (req, res, next) {
-  req.app.locals.db
-    .collection("media")
-    .deleteOne({ _id: ObjectId(req.body._id) })
-    .then((result) => {
-      console.log(result);
-      res.send(result);
-    });
+router.post("/delete", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .deleteOne({ _id: ObjectId(req.body._id) })
+      .then((result) => {
+        console.log(result);
+        res.send(result);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
 //Change post
-router.post("/change", function (req, res, next) {
-  console.log(req.body);
-  req.app.locals.db
-    .collection("media")
-    .updateMany(
-      {
-        _id: ObjectId(req.body.media._id),
-      },
-      {
-        $set: {
-          title: req.body.media.title,
-          description: req.body.media.description,
-          media_url: req.body.media.media_url,
-          type: req.body.media.type,
-          date_pub: req.body.media.date_pub,
+router.post("/change", async function (req, res, next) {
+  try {
+    await req.app.locals.db
+      .collection("media")
+      .updateMany(
+        {
+          _id: ObjectId(req.body.media._id),
         },
-      }
-    )
-    .then((result) => {
-      console.log(result);
-      res.send(result);
-    });
+        {
+          $set: {
+            title: req.body.media.title,
+            description: req.body.media.description,
+            media_url: req.body.media.media_url,
+            type: req.body.media.type,
+            date_pub: req.body.media.date_pub,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        res.send(result);
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: "Could not perform the task" });
+  }
 });
 
 module.exports = router;
