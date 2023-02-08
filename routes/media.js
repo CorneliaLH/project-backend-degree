0dd70c95396448249b4b19fd4d565b7d63eb1f88
Conrelia Harder <cornelialisaharder@gmail.com>
2023-02-08 11:03:27 +0100

    uppdating error response routes

--- routes/media.js
+++ routes/media.js
@@ -23,7 +23,7 @@ router.get("/", async function (req, res, next) {
           }
         }
         console.log(results);
-
+        throw new Error("Hello error!");
         res.send(showArray);
       });
   } catch (error) {
